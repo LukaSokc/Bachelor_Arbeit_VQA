dc977db02495f36ef22c1150dc7a083d2c50df0c
arzi385 <137776902+arzi385@users.noreply.github.com>
2025-03-14 14:11:47 +0100

    Datensatz geholt
    get_data laufen lassen für die Daten

---
+++ temp/get_data.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+from pathlib import Path
+
+current_file = Path(__file__).resolve()
+
+project_root = current_file.parent.parent  # -> `Bachelor_Arbeit_VQA/`
+
+save_path = project_root / "data" / "raw"
+
+dataset = load_dataset("flaviagiammarino/path-vqa")
+
+dataset.save_to_disk(str(save_path))
+
